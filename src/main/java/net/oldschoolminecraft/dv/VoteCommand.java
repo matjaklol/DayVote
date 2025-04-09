548f7c64b6a7937b2f2500430fa1827a3479d9a3
SavageUser <LimitlessPowerHD@gmail.com>
2025-04-09 02:55:01 -0400

    Update VoteCommand.java
    Total command overhaul.

--- src/main/java/net/oldschoolminecraft/dv/VoteCommand.java
+++ src/main/java/net/oldschoolminecraft/dv/VoteCommand.java
@@ -1,96 +1,153 @@
 package net.oldschoolminecraft.dv;
 
-import com.earth2me.essentials.User;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
-public class VoteCommand implements CommandExecutor
-{
+public class VoteCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-    {
-        if (args.length > 0 && args[0].equalsIgnoreCase("reload") && (sender.isOp() || sender.hasPermission("DayVote.Reload")))
-        {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length > 0 && args[0].equalsIgnoreCase("help") || args.length > 0 && args[0].equalsIgnoreCase("?")) {
+            if (sender.isOp() || sender.hasPermission("DayVote.StaffHelp")) {
+                sender.sendMessage("§aDayVote §7version §b" + DayVote.getInstance().getDescription().getVersion());
+                sender.sendMessage("§eCommands:");
+                sender.sendMessage("§d/vote day §8- §7Starts a vote for day.");
+                sender.sendMessage("§d/vote help §8- §7Reveals this help page.");
+                sender.sendMessage("§d/vote info §8- §7Displays plugin information.");
+                sender.sendMessage("§d/vote reload §8- §7Reloads the config.yml.");
+                sender.sendMessage("§d/vote reset §8- §7Resets an active vote.");
+                sender.sendMessage("§d/vote <yes:no> §8- §7Casts a vote for day or night.");
+                return true;
+            }
+            else {
+                sender.sendMessage("§aDayVote §7version §b" + DayVote.getInstance().getDescription().getVersion());
+                sender.sendMessage("§eCommands:");
+                sender.sendMessage("§d/vote day §8- §7Starts a vote for day.");
+                sender.sendMessage("§d/vote help §8- §7Reveals this help page.");
+                sender.sendMessage("§d/vote info §8- §7Displays plugin information.");
+                sender.sendMessage("§d/vote <yes:no> §8- §7Casts a vote for day or night.");
+                return true;
+            }
+        }
+
+        if (args.length > 0 && args[0].equalsIgnoreCase("info")) {
+            sender.sendMessage("§aDayVote §7version §b" + DayVote.getInstance().getDescription().getVersion());
+            sender.sendMessage("§7Website: §egithub.com/OldSchoolMinecraft/DayVote");
+            sender.sendMessage("§7Authors(s): §emoderator_man");
+            sender.sendMessage("§7Contributors(s): §ematjaklol§7, §eSavageUser§7");
+            return true;
+        }
+
+        if (args.length > 0 && args[0].equalsIgnoreCase("reload") && (sender.isOp() || sender.hasPermission("DayVote.Reload"))) {
             DayVote.getInstance().getConfig().reload();
-            sender.sendMessage(ChatColor.GREEN + "Reloaded configuration");
+            sender.sendMessage("§aReloaded §bconfig.yml§a!");
             return true;
         }
 
-        if (args.length > 0 && args[0].equalsIgnoreCase("reset") && (sender.isOp() || sender.hasPermission("DayVote.Reset")))
-        {
-            DayVote.getInstance().processVote();
-            sender.sendMessage(ChatColor.GREEN + "Reset vote status");
-            return true;
+        if (args.length > 0 && args[0].equalsIgnoreCase("reset") && (sender.isOp() || sender.hasPermission("DayVote.Reset"))) {
+            Vote vote = DayVote.getInstance().getActiveVote();
+
+            if (vote == null) {
+                sender.sendMessage("§4Theres no active vote to reset!");
+                return true;
+            }
+            else {
+                DayVote.getInstance().processVote();
+                sender.sendMessage("§aCurrent vote §bterminated§a!");
+                return true;
+            }
         }
 
-        if (!(sender instanceof Player))
-        {
-            sender.sendMessage(ChatColor.RED + "Only players can use this command!");
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("§4Command can only be executed by players!");
             return true;
         }
 
         Vote vote = DayVote.getInstance().getActiveVote();
 
-        if (args.length == 0)
-        {
-            if (vote == null) // no vote active
-                sender.sendMessage(ChatColor.RED + "No vote is active. Use: " + ChatColor.GRAY + "/vote day" + ChatColor.RED + " to start a vote.");
-            else sender.sendMessage(ChatColor.RED + "A vote is currently active. Use: " + ChatColor.GRAY + "/vote <yes/no>" + ChatColor.RED + " to vote.");
-            return true;
-        }
-
-        if (args[0].equalsIgnoreCase("day"))
-        {
-            if (vote != null) countYesVote(vote, (Player)sender);
+        if (args.length == 0) {
+            if (vote == null) { // no vote active
+                sender.sendMessage("§4No vote is active! Use §b/vote day §4to start a vote.");
+                return true;
+            }
             else {
-                vote = DayVote.getInstance().startNewVote();
-                if (vote == null) // cooldown prevented new vote start
-                {
-                    sender.sendMessage(ChatColor.RED + "You have to wait to start another vote.");
-                    return true;
-                }
-                countYesVote(vote, (Player)sender);
+                int required = (int) DayVote.getInstance().getConfig().getConfigOption("yesVotePercentageRequired");
+
+                sender.sendMessage("§1[§bOSM§1] §3Time Remaining: §b" + DayVote.getInstance().formatTime(DayVote.getInstance().getVoteTimeLeft()));
+                sender.sendMessage("§1[§bOSM§1] §3Required: §e" + required + "%");
+                sender.sendMessage("§1[§bOSM§1] §3Current Results: §a" + vote.getYesVotes() + "%§8/§4" + vote.getNoVotes() + "%");
+                sender.sendMessage("§1[§bOSM§1] §3Vote for day or night using §b/vote yes §3or §b/vote no§3.");
+                return true;
             }
         }
 
-        if (args[0].equalsIgnoreCase("yes"))
-        {
+        if (args[0].equalsIgnoreCase("day")) {
+            if (vote != null) {
+                countYesVote(vote, (Player)sender);
+                return true;
+            }
+            else {
+                vote = DayVote.getInstance().startNewVote();
+                if (vote == null) { // cooldown prevented new vote start
+                    sender.sendMessage("§4Cooldown time left: §b" + DayVote.getInstance().formatTime(DayVote.getInstance().getCooldownTimeLeft()));
+                    return true;
+                }
+                countYesVote(vote, (Player)sender);
+                return true;
+            }
+        }
+
+        if (args[0].equalsIgnoreCase("yes") || args[0].equalsIgnoreCase("y")) {
             if (vote != null) countYesVote(vote, (Player)sender);
-            else sender.sendMessage(ChatColor.RED + "No vote is active. Use: " + ChatColor.GRAY + "/vote day" + ChatColor.RED + " to start a vote.");
+            else sender.sendMessage("§4No vote is active! Use §b/vote day §4to start a vote.");
+            return true;
         }
 
-        if (args[0].equalsIgnoreCase("no"))
-        {
+        if (args[0].equalsIgnoreCase("no") || args[0].equalsIgnoreCase("n")) {
             if (vote != null) countNoVote(vote, (Player)sender);
-            else sender.sendMessage(ChatColor.RED + "No vote is active. Use: " + ChatColor.GRAY + "/vote day" + ChatColor.RED + " to start a vote.");
+            else sender.sendMessage("§4No vote is active! Use §b/vote day §4to start a vote.");
+            return true;
         }
-
-        return true;
+        else {
+            if (sender.isOp() || sender.hasPermission("DayVote.StaffHelp")) {
+                sender.sendMessage("§aDayVote §7version §b" + DayVote.getInstance().getDescription().getVersion());
+                sender.sendMessage("§eCommands:");
+                sender.sendMessage("§d/vote day §8- §7Starts a vote for day.");
+                sender.sendMessage("§d/vote help §8- §7Reveals this help page.");
+                sender.sendMessage("§d/vote info §8- §7Displays plugin information.");
+                sender.sendMessage("§d/vote reload §8- §7Reloads the config.yml.");
+                sender.sendMessage("§d/vote reset §8- §7Resets an active vote.");
+                sender.sendMessage("§d/vote <yes:no> §8- §7Casts a vote for day or night.");
+                return true;
+            }
+            else {
+                sender.sendMessage("§aDayVote §7version §b" + DayVote.getInstance().getDescription().getVersion());
+                sender.sendMessage("§eCommands:");
+                sender.sendMessage("§d/vote day §8- §7Starts a vote for day.");
+                sender.sendMessage("§d/vote help §8- §7Reveals this help page.");
+                sender.sendMessage("§d/vote info §8- §7Displays plugin information.");
+                sender.sendMessage("§d/vote <yes:no> §8- §7Casts a vote for day or night.");
+                return true;
+            }
+        }
     }
 
-    private void countYesVote(Vote vote, Player ply)
-    {
-        if (vote.hasVoted(ply))
-        {
-            ply.sendMessage(ChatColor.RED + "You have already voted.");
+    private void countYesVote(Vote vote, Player player) {
+        if (vote.hasVoted(player)) {
+            player.sendMessage("§4You can only vote once!");
             return;
         }
-        vote.incrementYes(ply);
-        ply.sendMessage(ChatColor.GRAY + "Your vote has been counted.");
+        vote.incrementYes(player);
+        player.sendMessage("§fYour vote has been counted!");
     }
 
-    private void countNoVote(Vote vote, Player ply)
-    {
-        if (vote.hasVoted(ply))
-        {
-            ply.sendMessage(ChatColor.RED + "You have already voted.");
+    private void countNoVote(Vote vote, Player player) {
+        if (vote.hasVoted(player)) {
+            player.sendMessage("§4You can only vote once!");
             return;
         }
-        vote.incrementNo(ply);
-        ply.sendMessage(ChatColor.GRAY + "Your vote has been counted.");
+        vote.incrementNo(player);
+        player.sendMessage("§fYour vote has been counted!");
     }
 }
