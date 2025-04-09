2a3e8c4dd01c01795647012dd2b491e064195374
SavageUser <LimitlessPowerHD@gmail.com>
2025-04-09 02:50:51 -0400

    Update DayVote.java
    New methods added for tracking cooldown and vote time. Removed unused Essentials API.

--- src/main/java/net/oldschoolminecraft/dv/DayVote.java
+++ src/main/java/net/oldschoolminecraft/dv/DayVote.java
@@ -1,106 +1,124 @@
 package net.oldschoolminecraft.dv;
 
-import com.earth2me.essentials.Essentials;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class DayVote extends JavaPlugin
-{
+public class DayVote extends JavaPlugin {
+
+
     private static DayVote instance;
 
     private VoteConfig config;
     private Vote vote;
     private long lastVote;
-    private Essentials essentials;
+    private long lastStartVote;
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
-    public void onEnable()
-    {
+    @Override
+    public void onEnable() {
         instance = this;
         config = new VoteConfig(new File(getDataFolder(), "config.yml"));
         lastVote = UnixTime.now() - 250;
         getCommand("vote").setExecutor(new VoteCommand());
 
-        System.out.println("DayVote started.");
-        System.out.println("Set last vote time to: " + lastVote);
-        System.out.println("Current unix time: " + UnixTime.now());
-        System.out.println("Vote cooldown: " + config.getConfigOption("cooldownSeconds"));
+        System.out.println("DayVote version: "+ getDescription().getVersion() + " enabled!");
+        System.out.println("Last Vote Time: " + lastVote);
+        System.out.println("Current Unix Time: " + UnixTime.now());
+        System.out.println("Vote Cooldown Setting: " + config.getConfigOption("cooldownSeconds"));
         System.out.println("Can start vote? " + (canStartVote() ? "Yes" : "No"));
     }
 
-    public void onDisable() {}
+    @Override
+    public void onDisable() {
+        forceCancelVote();
+        System.out.println("DayVote version: "+ getDescription().getVersion() + " disabled!");
+    }
 
-    public Vote getActiveVote()
-    {
+    public Vote getActiveVote() {
         return vote;
     }
 
-    public synchronized boolean canStartVote()
-    {
+    public synchronized boolean canStartVote() {
         long timeSinceLastVote = (UnixTime.now() - lastVote);
         int cooldown = (int) config.getConfigOption("cooldownSeconds");
         return timeSinceLastVote >= cooldown;
     }
 
-    public synchronized Vote startNewVote()
-    {
+    public int getCooldownTimeLeft() {
+        long timeSinceLastVote = (UnixTime.now() - lastVote);
+        int cooldown = (int) config.getConfigOption("cooldownSeconds");
+        return (int) (cooldown-timeSinceLastVote);
+    }
+
+    public int getVoteTimeLeft() {
+        long timeSinceLastVoteStart = (UnixTime.now() - lastStartVote);
+        int voteDurationSeconds = (int) config.getConfigOption("voteDurationSeconds");
+        return (int) (voteDurationSeconds-timeSinceLastVoteStart);
+    }
+
+    public String formatTime(final long seconds) {
+        final long minute = TimeUnit.SECONDS.toMinutes(seconds);
+        final long second = TimeUnit.SECONDS.toSeconds(seconds) - TimeUnit.SECONDS.toMinutes(seconds) * 60L;
+        return minute + "m" + second + "s";
+    }
+
+    public synchronized Vote startNewVote() {
         if (!canStartVote()) return null;
         vote = new Vote();
         broadcast(String.valueOf(config.getConfigOption("messages.started")));
         int voteDurationSeconds = (int) config.getConfigOption("voteDurationSeconds");
 //        Bukkit.getScheduler().scheduleAsyncDelayedTask(this, this::processVote, voteDurationSeconds * 20L);
         scheduler.schedule(this::processVote, voteDurationSeconds, TimeUnit.SECONDS);
+        lastStartVote = UnixTime.now();
         return vote;
     }
 
-    public synchronized void processVote()
-    {
-        if (vote == null)
-        {
+    public synchronized void processVote() {
+        if (vote == null) {
             startNewVote();
             return;
         }
-        if (vote.didVotePass())
-        {
+
+        if (vote.didVotePass()) {
             broadcast(String.valueOf(config.getConfigOption("messages.succeeded")));
-            getServer().getWorld("world").setTime(0L);
-        } else broadcast(String.valueOf(config.getConfigOption("messages.failed")));
+            Bukkit.getServer().getWorld("world").setTime(0);
+        }
+        else {
+            broadcast(String.valueOf(config.getConfigOption("messages.failed")));
+        }
         resetVote();
     }
 
-    private synchronized void resetVote()
-    {
+
+    private synchronized void resetVote() {
         vote = null;
         lastVote = UnixTime.now();
     }
 
-    private void broadcast(String msg)
-    {
-        for (Player ply : getServer().getOnlinePlayers())
-            ply.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+    private synchronized void forceCancelVote() {
+        if (vote != null) {
+            resetVote();
+        }
     }
 
-    public VoteConfig getConfig()
-    {
+    private void broadcast(String msg) {
+        for (Player all : getServer().getOnlinePlayers())
+            all.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+    }
+
+    public VoteConfig getConfig() {
         return config;
     }
 
-    public Essentials getEssentials()
-    {
-        return essentials;
-    }
-
-    public static DayVote getInstance()
-    {
+    public static DayVote getInstance() {
         return instance;
     }
 }
+
